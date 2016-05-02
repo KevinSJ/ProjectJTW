e5a3d25f0df792b4aafee2bcac802319c18944e9
Kevin <kevin4313@163.com>
2016-05-02 00:22:09 -0500

    update sql

---
+++ Relations.sql
@@ -0,0 +1,17 @@
+﻿-- Table: public."Relations"
+
+-- DROP TABLE public."Relations";
+
+CREATE TABLE public."Relations"
+(
+  relation character varying,
+  source integer,
+  target integer,
+  id integer NOT NULL,
+  CONSTRAINT "Relations_pkey" PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public."Relations"
+  OWNER TO postgres;
---
+++ XiyoujiDB.sql
@@ -0,0 +1,12 @@
+﻿-- Database: "XiyoujiDB"
+
+-- DROP DATABASE "XiyoujiDB";
+
+CREATE DATABASE "XiyoujiDB"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Chinese (Simplified)_People''s Republic of China.936'
+       LC_CTYPE = 'Chinese (Simplified)_People''s Republic of China.936'
+       CONNECTION LIMIT = -1;
+
